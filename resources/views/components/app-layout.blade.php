7260c779ba592f83ba5935786fbf270b8b1c8181
nursyah <nursyahjaya@gmail.com>
2023-06-28 14:22:43 +0700

    get data from php in javascript

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -3,17 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Jalan Hijau</title>
+        <!-- @php
+            $title = config('app.name', 'Laravel');
+            if (isset($titlePage)) {
+                $title = $titlePage . ' | ' . $title;
+            }
+        @endphp -->
+        <title>{{ config('app.name', 'Laravel') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
 
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-      <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <!-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-          </div> -->
-        {{ $slot }}
-      </div>
+    <body>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
+        @yield('scripts')
     </body>
 </html>
